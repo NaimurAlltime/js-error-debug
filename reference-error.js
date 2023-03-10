0b4cf2350622e7220e15f415ef8c1d3bfe3f44f5
Naimur Rahman <naimur2231@gmail.com>
2023-03-10 10:43:19 +0600

    reference error in js

---
+++ reference-error.js
@@ -0,0 +1,11 @@
+const x = 3;
+const y = 5;
+// console.log(z)
+
+// console.log(add(2,3))
+function add(num1, num2){
+    return num1 + num2;
+}
+
+// console.log(add2(2,3))
+const add2 = (num1, num2) => num1 + num2;
