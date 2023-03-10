d468ed724da391e45ef3dadf35ddebf68598a6cf
Naimur Rahman <naimur2231@gmail.com>
2023-03-10 10:24:35 +0600

    syntex error js

---
+++ syntex-error.js
@@ -0,0 +1,29 @@
+function isTrue(num) {
+    if (num > 5) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+// }
+
+const result = isTrue(5);
+// console.log(result)
+
+// const sum = a, b => a+b;
+const sum = (a, b) => a+b;
+const res = sum(3, 4);
+// console.log(res);
+
+const Student = {
+    name: 'naim',
+    // age: 22
+    age: 22,
+    address: 'gazipur'
+}
+
+// console.log(Student.age);
+
+// for(i=1; i <10 i++){}
+for(i=1; i <10; i++){}
