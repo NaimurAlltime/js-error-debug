e10b1579214124b4dd71faf7356af09f4452afef
Naimur Rahman <naimur2231@gmail.com>
2023-03-10 10:36:23 +0600

    type error in js

---
+++ type-error.js
@@ -0,0 +1,11 @@
+// const nums = 5;
+// nums.push(34);
+// console.log(nums)
+
+// const Student;
+// console.log(Student.name);
+
+
+const Student = {name: 'naim', age: 22}
+// console.log(Student.address.name);
+console.log(Student.address?.name);
